f7fd0fcd229112d60d5d52e336c332bfbcba2019
Abhishek Singhal <abhisheksinghal8086@gmail.com>
2017-05-20 01:30:08 +0530

    MobileIron Interview

---
+++ MobileIron.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class MobileIron {
+	final static int processTypeLength = 6;
+	final static int processValueLength = 4;
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNextLine()) {
+			String line = sc.nextLine();
+			int offset = 0;
+			String type = line.substring(offset, offset + processTypeLength);
+			offset += processTypeLength + 1;
+			int length = Integer.parseInt(line.substring(offset, offset + 4));
+			offset += processValueLength + 1;
+			String value = line.substring(offset, offset + length);
+			offset += length + 1;
+			switch (type) {
+			case "UPPRCS":
+				System.out.println("UPPRCS-" + value.toUpperCase());
+				break;
+			case "REPLCE":
+				System.out.println("REPLCE-THIS STRING");
+				break;
+			default:
+				System.out.println("Type not valid");
+			}
+		}
+	}
+}
