247c3f07df90b9d323ef36a981e728905744b093
Abhishek Singhal <abhisheksinghal8086@gmail.com>
2017-05-23 17:52:21 +0530

    added new function grab challenge2 using dp

--- grabChallenge2.cpp
+++ grabChallenge2.cpp
@@ -18,13 +18,32 @@ if(day>=31)
       return minCost(hash,day+1);
 }
 
+int minCostDp(int hash[]){
+int dp[31]={0};
+for(int i=1;i<31;i++){
+  if(hash[i]){
+    if(i<7){
+      dp[i]=min(2+dp[i-1],7);
+    }
+    else{
+      dp[i]=min(2+dp[i-1],7+dp[i-7]);
+    }
+  }
+  else{
+    dp[i]=dp[i-1];
+  }
+}
+return min(dp[30],25);
+}
+
 int solution(vector<int> &A) {
     // write your code in C++14 (g++ 6.2.0)
     int hash[31]={0};
     for(int i=0;i<A.size();i++){
      hash[A[i]]=1;   
     }
-    cout<<min(minCost(hash,1),25)<<endl;
+    //cout<<min(minCost(hash,1),25)<<endl;
+    cout<<minCostDp(hash)<<endl;
     return 0;
 }
 
@@ -36,9 +55,10 @@ A[0] = 1;
     A[1] = 2;
     A[2] = 4;
     A[3] = 5;
-    A[4] = 7;
-    A[5] = 29;
-    A[6] = 30;
+    A[4] = 6;
+    //A[5] = 29;
+    //A[6] = 30;
+
     solution(A);
   return 0;
 }
